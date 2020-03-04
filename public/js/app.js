5c4f05e13d946ba9936f11ff63dd84f4ce4eabb7
Ray-Bags <raymondbaguma2@gmail.com>
2020-03-04 10:34:33 +0100

    update UX

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const messageTwo = document.querySelector('#message2')
 weatherForm.addEventListener('submit', (e) => {
    e.preventDefault()
 
-   const location = search.value
+   const location = search.value;
 
    messageOne.textContent = 'Loading...'
    messageTwo.textContent = ''
@@ -15,10 +15,10 @@ weatherForm.addEventListener('submit', (e) => {
    fetch('/weather?address=' + location).then((response) => {
       response.json().then((data) => {
          if (data.error) {
-            messageOne.textContent = data.error
+            messageOne.textContent = data.error;
          } else {
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
          }
       })
    })
